d4d34f5a651f0178e09ab3825c965f419ebf7cbe
Sunarto <sunarto.8735@admin.smk.belajar.id>
2026-06-09 04:27:53 +0700

    Update Jurusan, visi misi dan Tentang Kami

---
+++ load.php
@@ -0,0 +1,19 @@
+<?php
+
+header('Content-Type: text/html; charset=utf-8');
+
+$json = json_decode(
+    file_get_contents('data/jurusan.json'),
+    true
+);
+
+$key = $_GET['page'] ?? '';
+
+if(isset($json[$key]))
+{
+    echo file_get_contents($json[$key]);
+}
+else
+{
+    echo "<h3>Data tidak ditemukan</h3>";
+}
